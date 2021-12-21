514062b1dab2f17d3da7de82c10ac182fdf5f851
Jasper Ginn <jasperginn@gmail.com>
2021-12-21 19:23:13 +0100

    add dask distributed wrappers of utility functions

--- notebooks/dask_utils.py
+++ notebooks/dask_utils.py
@@ -1,12 +1,15 @@
 from pathlib import Path
-from typing import Any, Tuple, Union
+from typing import Any, List, Tuple, Union
 
+import dask
 import numpy as np
+import toolz
 import torch
 from nptyping import NDArray
 from PIL import Image
 from torchtyping import TensorType
 from torchvision import transforms
+from torchvision.models.resnet import ResNet
 
 
 def load_image(path: Union[str, Path]) -> Image.Image:
@@ -28,15 +31,55 @@ def preprocess_image(
 
 
 def stack_batch(
-    batch: Tuple[TensorType[224, 224, 3, torch.float64]]
-) -> TensorType[-1, 224, 224, 3, torch.float64]:
+    batch: Tuple[TensorType[3, 224, 224, torch.float64]]
+) -> TensorType[-1, 3, 224, 224, torch.float64]:
     return torch.stack(batch)
 
 
 def predict(
-    batch: TensorType[3, 224, 224, -1, torch.float64], model
+    batch: TensorType[3, 224, 224, -1, torch.float64], model: ResNet
 ) -> NDArray[(Any,), np.float64]:
     with torch.no_grad():
         out = model(batch)
-        _, predicted = torch.max(out, 1)
-    return predicted.numpy()
+    return out.numpy()
+
+
+@dask.delayed
+def load_image_wrapper(
+    img_path: Union[Path, str]
+) -> NDArray[(1, Any, Any, 3), np.uint8]:
+    return load_image(img_path)
+
+
+@dask.delayed
+def preprocess_image_wrapper(
+    img: NDArray[(Any, Any, 3), np.uint8]
+) -> TensorType[3, 224, 224, torch.float64]:
+    return preprocess_image(img)
+
+
+@dask.delayed
+def stack_batch_wrapper(
+    batch: Tuple[TensorType[3, 224, 224, torch.float64]]
+) -> TensorType[-1, 3, 224, 224, torch.float64]:
+    return stack_batch(batch)
+
+
+@dask.delayed
+def predict_wrapper(
+    batch: TensorType[-1, 3, 224, 224, torch.float64],
+    model: ResNet,
+    return_class: bool = False,
+) -> NDArray[(Any,), np.float64]:
+    if not return_class:
+        return predict(batch, model)
+    else:
+        return np.argmax(predict(batch, model), axis=1)
+
+
+def create_batches(
+    data: List[TensorType[3, 224, 224, torch.float64]], batch_size: int = 10
+) -> TensorType[-1, 3, 224, 224, torch.float64]:
+    return [
+        stack_batch_wrapper(batch) for batch in toolz.partition_all(batch_size, data)
+    ]
